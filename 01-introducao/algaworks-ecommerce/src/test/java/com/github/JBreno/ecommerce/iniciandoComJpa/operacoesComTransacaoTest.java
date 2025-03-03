2a41f12d80437611b6f8020488d456ab106021ca
J-Breno <joao.breno85@hotmail.com>
2025-03-03 17:54:06 -0300

    atualizando o banco de dados com merge

--- 01-introducao/algaworks-ecommerce/src/test/java/com/github/JBreno/ecommerce/iniciandoComJpa/operacoesComTransacaoTest.java
+++ 01-introducao/algaworks-ecommerce/src/test/java/com/github/JBreno/ecommerce/iniciandoComJpa/operacoesComTransacaoTest.java
@@ -10,6 +10,26 @@ import java.math.BigDecimal;
 public class operacoesComTransacaoTest extends EntityManagerTest {
 
     @Test
+    public void atualizarObjeto() {
+        Produto produto = new Produto();
+
+        produto.setId(1);
+        produto.setNome("Kindle PaperWhite");
+        produto.setDescricao("Conheça o novo Kindle.");
+        produto.setPreco(new BigDecimal(599.0));
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(produto);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, 1);
+        Assert.assertNotNull(produtoVerificacao);
+        Assert.assertEquals("Kindle PaperWhite", produtoVerificacao.getNome());
+    }
+
+    @Test
     public void removerObjeto() {
         Produto produto = entityManager.find(Produto.class, 3);
 
