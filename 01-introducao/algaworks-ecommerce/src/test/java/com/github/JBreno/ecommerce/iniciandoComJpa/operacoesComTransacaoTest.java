9f8c3b77b84e2f01ba85b55edbe06c02ea6acf83
J-Breno <joao.breno85@hotmail.com>
2025-03-03 17:16:34 -0300

    Abrindo e fechando uma transacao

---
+++ 01-introducao/algaworks-ecommerce/src/test/java/com/github/JBreno/ecommerce/iniciandoComJpa/operacoesComTransacaoTest.java
@@ -0,0 +1,22 @@
+package com.github.JBreno.ecommerce.iniciandoComJpa;
+
+import com.github.JBreno.ecommerce.EntityManagerTest;
+import com.github.JBreno.ecommerce.model.Produto;
+import org.junit.Test;
+
+public class operacoesComTransacaoTest extends EntityManagerTest {
+
+    @Test
+    public void abrirFecharTransacao() {
+        Produto produto = new Produto(); // para o método não mostrar erros
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(produto);
+        entityManager.merge(produto);
+        entityManager.remove(produto);
+
+        entityManager.getTransaction().commit();
+    }
+
+}
