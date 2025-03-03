0eca556eabaab2711ec57773cad7c10cbc00dddd
J-Breno <joao.breno85@hotmail.com>
2025-03-03 15:42:11 -0300

    Busca por identificador

--- 01-introducao/algaworks-ecommerce/src/test/java/com/github/JBreno/ecommerce/iniciandoComJpa/ConsultandoRegistrosTest.java
+++ 01-introducao/algaworks-ecommerce/src/test/java/com/github/JBreno/ecommerce/iniciandoComJpa/ConsultandoRegistrosTest.java
@@ -1,9 +1,7 @@
 package com.github.JBreno.ecommerce.iniciandoComJpa;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import com.github.JBreno.ecommerce.model.Produto;
+import org.junit.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -31,5 +29,23 @@ public class ConsultandoRegistrosTest {
     @After
     public void tearDown() {
         entityManager.close();
+    }
+
+    @Test
+    public void buscarPorIdentificador(){
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Assert.assertNotNull(produto);
+        Assert.assertEquals("Kindle", produto.getNome());
+    }
+
+    @Test
+    public void atualizarReferencia() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setNome("Microfone Samson");
+
+        entityManager.refresh(produto);
+
+        Assert.assertEquals("Kindle", produto.getNome());
     }
 }
