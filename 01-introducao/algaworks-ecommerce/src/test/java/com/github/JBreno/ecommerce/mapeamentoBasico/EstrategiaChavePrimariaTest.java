576072c7bb3b5a7c4c760a54481ffa9329bbd2a3
J-Breno <joao.breno85@hotmail.com>
2025-03-03 22:23:19 -0300

    estrategias para chave primaria com generatedValue

---
+++ 01-introducao/algaworks-ecommerce/src/test/java/com/github/JBreno/ecommerce/mapeamentoBasico/EstrategiaChavePrimariaTest.java
@@ -0,0 +1,25 @@
+package com.github.JBreno.ecommerce.mapeamentoBasico;
+
+import com.github.JBreno.ecommerce.EntityManagerTest;
+import com.github.JBreno.ecommerce.model.Categoria;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EstrategiaChavePrimariaTest extends EntityManagerTest {
+
+    @Test
+    public void testarEstrategiaAuto() {
+        Categoria categoria = new Categoria();
+        categoria.setNome("Eletrônicos");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoria);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assert.assertNotNull(categoriaVerificacao);
+
+    }
+}
